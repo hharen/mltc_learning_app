ca1d6c4e0b89775d6ed8cd336c92a454ee414028
Hana Harencarova <h.harencarova@gmail.com>
2021-11-08 20:53:57 +0100

    Add ordering to lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class Lesson < ApplicationRecord
+  default_scope { order(:order) }
+
   has_many :materials
   belongs_to :topic
 
   delegate :course, to: :topic
 
   validates :name, presence: true
+
+  before_save :reorder
+
+  private
+
+  def reorder
+    lessons_to_reorder = Lesson.where(topic_id: self.topic_id, order: (self.order..))
+    lessons_to_reorder.reverse.each do |lesson|
+      lesson.update_column(:order, lesson.order+1)
+    end
+  end
 end
