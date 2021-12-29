47e7b0231119c1d63ff5042961995002281e30b9
Hana Harencarova <h.harencarova@gmail.com>
2021-12-29 23:09:11 +0100

    Add start course button

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,5 +7,6 @@ class CoursesController < ApplicationController
 
   def show
     @course = Course.find(params[:id])
+    # @subscription = Subscription.where(course: @course, user: current_user)
   end
 end
