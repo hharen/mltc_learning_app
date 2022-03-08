5a84c1219b9815c27e65947f76ea7c8174f603f5
Hana Harencarova <h.harencarova@gmail.com>
2022-03-08 13:44:20 +0100

    Add action text to materials form

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -5,22 +5,17 @@ class MaterialsController < AdminController
     @materials = Material.where(lesson_id: params[:lesson_id])
     @lesson_name = Lesson.find(params[:lesson_id]).name
     @material = Material.new(lesson_id: params[:lesson_id])
-    @path = new_lesson_material_path
+    @path = lesson_materials_path
   end
 
   def new
     @material = Material.new(lesson_id: params[:lesson_id])
-    @path = new_lesson_material_path
+    @path = lesson_materials_path
   end
 
   def create
     @material = Material.new(type: params[:material][:type], lesson_id: params[:material][:lesson_id])
-    # binding.pry
-    if ['TextMaterial', 'TaskMaterial', 'ResourceMaterial'].include?(@material.type)
-      @material.body = params[:material][:body_rich_text]
-    else
-      @material.body = params[:material][:body_plain]
-    end
+    resolve_body(@material)
 
     if @material.save
       redirect_to lesson_materials_path(@material.lesson, @material)
@@ -38,7 +33,10 @@ class MaterialsController < AdminController
     material = Material.find(params[:id])
     @path = material_path(material)
 
-    if material.update(material_params)
+    material.order = params[:material][:order]
+    resolve_body(material)
+
+    if material.save
       redirect_to lesson_materials_path(material.lesson, material)
     else
       render 'edit'
@@ -46,19 +44,27 @@ class MaterialsController < AdminController
   end
 
   def destroy
-    @material = Material.find(params[:id])
+    material = Material.find(params[:id])
 
-    if @material.destroy
-      flash[:success] = 'The @material was removed.'
+    if material.destroy
+      flash[:success] = 'The material was removed.'
     else
       flash[:error] = "Unfortunately, the material couldn't be deleted."
     end
-    redirect_to lesson_materials_path(@material.lesson, @material)
+    redirect_to lesson_materials_path(material.lesson, material)
   end
 
   private
 
   def material_params
     params.require(:material).permit(:type, :body, :order, :lesson_id)
+  end
+
+  def resolve_body(material)
+    material.body = if %w[TextMaterial TaskMaterial ResourceMaterial].include?(material.type)
+                      params[:material][:body_rich_text]
+                    else
+                      params[:material][:body_plain]
+                    end
   end
 end
