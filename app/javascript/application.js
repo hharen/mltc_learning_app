d014155a17752db322624e97ff445d98e2b5718c
Hana Harencarova <h.harencarova@gmail.com>
2022-03-14 19:52:42 +0100

    Open links outside of the domain in a new window

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,12 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+// Open all external links in a new window
+addEventListener("click", function(event) {
+  var el = event.target
+
+  if (el.tagName === "A" && !el.isContentEditable && el.host !== window.location.host) {
+    el.setAttribute("target", "_blank")
+  }
+}, true)
