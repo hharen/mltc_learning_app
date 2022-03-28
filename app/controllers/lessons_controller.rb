d3afada8e23bfd1908ea14c35c159dacd9af5e2c
Hana Harencarova <h.harencarova@gmail.com>
2022-03-28 21:16:25 +0200

    Make a special notice type for notices with url

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,7 +8,7 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     @subscription = current_user.subscriptions.where(course: @lesson.course).first
     if @subscription.nil?
-      flash[:notice] = "You don't own #{@lesson.course.name}, go to #{helpers.link_to('www.momslearntocode.com', 'https://www.momslearntocode.com')} to get it."
+      flash[:notice_with_url] = t('lessons.get_course_html', course_name: @lesson.course.name, href: helpers.link_to('Moms Learn to Code', 'https://www.momslearntocode.com'))
       redirect_to subscriptions_path
     else
       @completed = @lesson.completed?(@subscription)
