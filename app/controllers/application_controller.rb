c66bab22c935d0b77c859c902825cad7cfbc6144
Hana Harencarova <h.harencarova@gmail.com>
2022-04-15 13:54:26 +0200

    Add redirect to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :redirect_subdomain
+
+  def redirect_subdomain
+    if request.host == 'momslearntocode.com'
+      redirect_to 'https://www.momslearntocode.com' + request.fullpath, :status => 301
+    end
+  end
 
   def configure_permitted_parameters
     update_attrs = %i[password password_confirmation current_password]
