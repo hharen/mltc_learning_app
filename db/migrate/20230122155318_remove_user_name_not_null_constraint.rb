2aa374fb64474bde5d2f7a6d06845961935f2309
Hana Harencarova <h.harencarova@gmail.com>
2023-01-22 16:59:36 +0100

    Add fly things

--- db/migrate/20230122155318_remove_user_name_not_null_constraint.rb
+++ db/migrate/20230122155318_remove_user_name_not_null_constraint.rb
@@ -2,6 +2,5 @@ class RemoveUserNameNotNullConstraint < ActiveRecord::Migration[7.1]
   def change
     change_column_null :users, :first_name, true
     change_column_null :users, :last_name, true
-
   end
 end
